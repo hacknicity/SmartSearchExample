2ba098431bc0b095da15f99f0a3b0e10fb36f641
Geoff Hackworth <geoff@hacknicity.com>
2021-01-23 14:50:41 +0000

    Initial Commit. Xcode 12.3's Single View App template. Removed my Team name from the Signing & Capabilities section. Set iOS Deployment Target to 14.0. Added .gitignore.

---
+++ SmartSearchExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartSearchExample
+//
+//  Created by Geoff Hackworth on 23/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
